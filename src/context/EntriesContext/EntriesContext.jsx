5b2583314c1751f89e13979ca1a64b491208c07c
Michael Rider <mikepdxrider@gmail.com>
2021-12-14 15:17:17 -0800

    adds custom hooks

--- src/context/EntriesContext/EntriesContext.jsx
+++ src/context/EntriesContext/EntriesContext.jsx
@@ -1,4 +1,5 @@
-import React, { createContext } from 'react'
+import React, { createContext, useContext, useState } from 'react'
+
 
 // Create a custom hook here
 // CreateContext and assign to variable.
@@ -6,11 +7,22 @@ const EntriesContext = createContext();
 
 // Define a functional component which recieves children as props, contains state, and returns a provider component with values.
 const EntriesProvider = ({children}) => {
-    const [state, setState] = useState([]);
+    const [entries, setEntries] = useState([]);
 
+    return(
+        <EntriesContext.Provider value={{entries, setEntries}}>
+            {children}
+        </EntriesContext.Provider>
+    )
 }
 
 // Create the hook function. 
+const useEntries = () => {
     //  pass the createdContext into useContext.
+    const context = useContext(EntriesContext);
     //  handle undefined context(components attempting to declare/access/use our custom hook without being inside of a provider)
+    if (context === undefined) throw new Error('useEntries must be declared within an EntriesProvider');
+    
     //  return context
+    return context;
+}
--- src/context/UserContext/UserContext.jsx
+++ src/context/UserContext/UserContext.jsx
@@ -1,12 +1,28 @@
-import React from 'react'
+import React, { createContext, useContext, useState } from 'react'
+
 
 // Create a custom hook here
-
 // CreateContext and assign to variable.
+const UserContext = createContext();
 
 // Define a functional component which recieves children as props, contains state, and returns a provider component with values.
+const UserProvider = ({children}) => {
+    const [user, setUser] = useState([]);
+
+    return(
+        <UserContext.Provider value={{user, setUser}}>
+            {children}
+        </UserContext.Provider>
+    )
+}
 
 // Create the hook function. 
+const useUser = () => {
     //  pass the createdContext into useContext.
+    const context = useContext(UserContext);
     //  handle undefined context(components attempting to declare/access/use our custom hook without being inside of a provider)
+    if (context === undefined) throw new Error('useUser must be declared within an UserProvider');
+    
     //  return context
+    return context;
+}
