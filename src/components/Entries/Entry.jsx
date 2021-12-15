73220a9bc9fca3a861aaef02659693eb7ed83e07
Michael Rider <mikepdxrider@gmail.com>
2021-12-14 16:04:11 -0800

    creates Entry component - shout out to Trianna & Simon from June

---
+++ src/components/Entries/Entry.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+// shout to Triana and Simon from June. I totally forgot about the possibility of an Entry component. This goes to show that good planning practices are critical to successful and complete development. I need to start doing more organizing and planning ahead of these projects.
+export default function Entry() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
