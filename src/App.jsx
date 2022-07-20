f70ab0334624df94c0f631f89951b49cd1aaa392
Michael Rider <mikepdxrider@gmail.com>
2022-07-19 17:34:07 -0700

    Add embed-sans-header

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
 export default function App() {
-  return <h1>Hello World</h1>;
+  return <>
+  
+  <h1>Embed Map test</h1>
+<p><iframe src="https://devl17.batchgeo.com/map/542bcdc29fdbfdb2e8fcf1a684b77045" frameborder="0" width="100%" height="550" sandbox="allow-top-navigation allow-scripts allow-popups allow-same-origin allow-modals allow-forms" style="border:1px solid #aaa;"></iframe></p><p><small>View <a href="https://devl17.batchgeo.com/map/542bcdc29fdbfdb2e8fcf1a684b77045">test_embed</a> in a full screen map</small></p></>;
 }
